5153e540edd649d8f02512f3753934d48b54f0ad
dsadsadsa2245 <daniel.zhanbolotov@uwis.edu.kg>
2023-08-10 19:43:03 +0600

    Добавил мой ip в send_mail

--- app/settings.py
+++ app/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR,'!!!!!!!!!!!!!!!!!!!!!!!!')
 
 
 # Quick-start development settings - unsuitable for production
@@ -26,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.125.174.221']
+ALLOWED_HOSTS = ['34.125.89.104','127.0.0.1']
 
 
 # Application definition
@@ -138,7 +139,7 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
